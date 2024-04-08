c1ff1eac6277bef934b7f20efe8904f331ce2186
Денис Харке <hda007@campus.mephi.ru>
2024-04-09 00:41:48 +0300

    add router and route programmatic navigation

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,10 +1,24 @@
 <template>
     <div>
         Home page
+
+        <button @click="buttonClicked">
+            Click me
+        </button>
     </div>
 </template>
 
 <script setup lang="ts">
+import { useRoute, useRouter } from 'vue-router';
+
+
+const route = useRoute()
+const router = useRouter()
+
+function buttonClicked() {
+    console.log('do work')
+    router.push({name: 'info'})
+}
 
 </script>
 
