9a0131ed1eeb2055aea0eeb9055397fd36ed329e
Денис Харке <hda007@campus.mephi.ru>
2024-04-10 00:03:32 +0300

    finish by adding primevue and axios

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,16 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import '@/assets/main.scss';
 import router from '@/router/router'
-
+import 'primevue/resources/themes/aura-light-green/theme.css'
+import PrimeVue from 'primevue/config';
+import Button from 'primevue/button'
 const app = createApp(App);
 
 
 app.use(router);
+app.use(PrimeVue);
+
+app.component('Button', Button);
 
 app.mount('#app');
 
--- src/views/posts.vue
+++ src/views/posts.vue
@@ -1,12 +1,28 @@
 <template>
-    <div>
-        Posts
+    <div class="posts-container">
+        <input type="number" v-model="postMaximum">
+        <Button label="name" @click="someAction"/>
+        <ul class="posts-list">
+            <li v-for="postItem in filtredArr" :key="postItem.id">
+                <div>{{ postItem.id }}</div>
+                <div>{{ postItem.body }}</div>
+            </li>
+        </ul>
     </div>
 </template>
 
 <script setup lang="ts">
 import axios from 'axios';
-import { onMounted, onUnmounted } from 'vue';
+import { computed, onMounted, onUnmounted, ref, watch } from 'vue';
+import type {Post} from '@/ interfaces/post'
+
+const postMaximum = ref(100)
+
+const posts = ref<Post[]>([])
+
+const someAction = () => {
+    console.log('work')
+}
 
 
 onMounted(async () => {
@@ -21,13 +37,34 @@ onMounted(async () => {
     //     console.log(e)
     // }
     
+    const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+    posts.value = response.data;
+    console.log(posts.value)
+    
 })
 
 onUnmounted(() => {
     console.log('unmount')
 })
+
+
+
+const filtredArr = computed(() => {
+    return posts.value.filter((value) => value.id < postMaximum.value)
+})
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+.posts-container {
+    margin-top: 5rem;
+}
 
+.posts-list {
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    li {
+        border: 1px solid red;
+    }
+}
 </style>
